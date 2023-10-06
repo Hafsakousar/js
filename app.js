9ff1888c6a855d6f176083d44246143dd2bb4e84
hafsa kousar <142086543+Hafsakousar@users.noreply.github.com>
2023-10-06 19:06:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// excercise:1.1
+alert("Hello World");
+console.log("hellow world");
+
+// excercise: 1.2
+alert("hii there");
+// excercise:1.3
+
+// excercise:1.4
+ let a=10;
+console.log(a=10);
